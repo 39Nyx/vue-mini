d4b158b311f1fd44f4b5a4ed3b7937d309b64865
hzj <hezhijian249@qq.com>
2025-09-03 23:34:33 +0800

    feat: baseHandlers实现

--- packages/reactivity/src/baseHandlers.ts
+++ packages/reactivity/src/baseHandlers.ts
@@ -1,1 +1,33 @@
- export const mutableHandlers: ProxyHandler<any> = {}
+import { track, trigger } from "./effect";
+
+function createGetter() {
+  return function get(target: any, key: string | symbol, receiver: any) {
+    const res = Reflect.get(target, key, receiver)
+    track(target, key)
+    return res
+  }
+}
+
+const get = createGetter()
+
+
+function createSetter() {
+  return function set(target: any, key: string | symbol, value: any, receiver: any) {
+    const res = Reflect.set(target, key, value, receiver)
+    trigger(target, key, value)
+    return res
+  }
+}
+
+const set = createSetter()
+
+export const mutableHandlers: ProxyHandler<any> = {
+  get,
+  set
+}
+
+function aa() {
+  return function abc() {
+    return 1
+  }
+}
